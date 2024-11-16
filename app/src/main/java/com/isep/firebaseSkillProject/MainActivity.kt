96e6411ba46d4f7d51415f9ffe7c247d885f60be
OrionVi1998 <15966899+OrionVi1998@users.noreply.github.com>
2024-11-16 19:15:06 +0100

    Create firestore accessors

--- app/src/main/java/com/isep/firebaseSkillProject/MainActivity.kt
+++ app/src/main/java/com/isep/firebaseSkillProject/MainActivity.kt
@@ -10,24 +10,17 @@ import com.google.firebase.Firebase
 import com.google.firebase.firestore.firestore
 
 class MainActivity : AppCompatActivity() {
+    private val db = Firebase.firestore
+    private var notes = emptyArray<Note>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
 
+        getNotes()
         // Access a Cloud Firestore instance from your Activity
 
-        val db = Firebase.firestore
 
-        db.collection("students")
-            .get()
-            .addOnSuccessListener { result ->
-                for (document in result) {
-                    Log.d("firebase-read","${document.id} => ${document.data}")
-                }
-            }
-            .addOnFailureListener { exception ->
-                Log.w("firebase-read", "Error getting documents.", exception)
-            }
 
         setContentView(R.layout.activity_main)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
@@ -36,4 +29,49 @@ class MainActivity : AppCompatActivity() {
             insets
         }
     }
+
+    private fun getNotes() {
+        db.collection("notes")
+            .get()
+            .addOnSuccessListener { result ->
+                notes =
+                    result
+                        .map { document -> Note(document.id, document.data["content"].toString()) }
+                        .toTypedArray()
+
+                for (document in result) {
+                    Log.d("firebase-getNotes", "${document.id} => ${document.data}")
+                }
+            }
+            .addOnFailureListener { exception ->
+                Log.w("firebase-getNotes", "Error getting documents.", exception)
+            }
+    }
+
+    private fun addNote(noteContent: String) {
+        db.collection("notes")
+            .add(hashMapOf("content" to noteContent))
+            .addOnSuccessListener { result ->
+                notes += Note(result.id, noteContent)
+                Log.d("firebase-addNote", "Added successfully")
+            }
+            .addOnFailureListener { exception ->
+                Log.w("firebase-addNote", "Error adding", exception)
+            }
+    }
+
+    private fun removeNotes(noteId: String) {
+        db.collection("notes")
+            .document(noteId)
+            .delete()
+            .addOnSuccessListener { result ->
+                notes = notes.filter { n -> n.id !== noteId }.toTypedArray()
+                Log.d("firebase-removeNotes", "Deleted successfully")
+            }
+            .addOnFailureListener { exception ->
+                Log.w("firebase-removeNotes", "Error deleting", exception)
+            }
+    }
 }
+
+class Note(val id: String, val content: String)

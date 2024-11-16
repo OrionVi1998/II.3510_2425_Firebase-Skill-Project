2c8585d7d5c770533289ed047805308cde08539c
OrionVi1998 <15966899+OrionVi1998@users.noreply.github.com>
2024-11-16 20:37:40 +0100

    Working add and note removal

--- app/src/main/java/com/isep/firebaseSkillProject/MainActivity.kt
+++ app/src/main/java/com/isep/firebaseSkillProject/MainActivity.kt
@@ -2,12 +2,16 @@ package com.isep.firebaseSkillProject
 
 import android.os.Bundle
 import android.util.Log
+import android.widget.Button
+import android.widget.EditText
+import android.widget.LinearLayout
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.google.firebase.Firebase
 import com.google.firebase.firestore.firestore
+
 
 class MainActivity : AppCompatActivity() {
     private val db = Firebase.firestore
@@ -16,13 +20,17 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
 
         getNotes()
-        // Access a Cloud Firestore instance from your Activity
 
+        val addNoteButton = findViewById<Button>(R.id.addNote)
+        addNoteButton.setOnClickListener {
+            val noteField = findViewById<EditText>(R.id.noteText)
+            addNote(noteField.getText().toString())
+            noteField.getText().clear()
+        }
 
-
-        setContentView(R.layout.activity_main)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
@@ -42,6 +50,8 @@ class MainActivity : AppCompatActivity() {
                 for (document in result) {
                     Log.d("firebase-getNotes", "${document.id} => ${document.data}")
                 }
+
+                buildNotes()
             }
             .addOnFailureListener { exception ->
                 Log.w("firebase-getNotes", "Error getting documents.", exception)
@@ -53,6 +63,7 @@ class MainActivity : AppCompatActivity() {
             .add(hashMapOf("content" to noteContent))
             .addOnSuccessListener { result ->
                 notes += Note(result.id, noteContent)
+                buildNotes()
                 Log.d("firebase-addNote", "Added successfully")
             }
             .addOnFailureListener { exception ->
@@ -60,17 +71,31 @@ class MainActivity : AppCompatActivity() {
             }
     }
 
-    private fun removeNotes(noteId: String) {
+    private fun removeNote(noteId: String) {
         db.collection("notes")
             .document(noteId)
             .delete()
             .addOnSuccessListener { result ->
                 notes = notes.filter { n -> n.id !== noteId }.toTypedArray()
+                buildNotes()
                 Log.d("firebase-removeNotes", "Deleted successfully")
             }
             .addOnFailureListener { exception ->
                 Log.w("firebase-removeNotes", "Error deleting", exception)
             }
+    }
+
+    private fun buildNotes() {
+        val container = findViewById<LinearLayout>(R.id.noteContainer)
+        container.removeAllViews()
+        for (note in notes) {
+            val noteButton = Button(this)
+            noteButton.width = LinearLayout.LayoutParams.MATCH_PARENT
+            noteButton.text = note.content
+            noteButton.setOnClickListener { removeNote(note.id) }
+
+            container.addView(noteButton)
+        }
     }
 }
 
